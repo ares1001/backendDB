8313231f60045c3c8d92271857a6a0385d2424b8
Ariel Galeppi <arielgaleppi@gmail.com>
2023-12-09 09:56:17 -0300

    9-12

---
+++ index.js
@@ -0,0 +1,41 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const path = require("path");
+const methodOverride = require("method-override");
+const sequelize = require('./src/models/connect');
+
+app.set("view engine" , "ejs");
+app.set("views", "./src/views");
+
+app.use(methodOverride('_method'));
+app.use(express.urlencoded({extended:false}))
+app.use(express.static(path.join(__dirname + "/public")));
+
+const mainRoutes = require('./src/routes/mainRoutes');
+app.use(mainRoutes);
+
+const adminProductosRoutes = require("./src/routes/admin/ProductosRoutes");
+
+app.use('/admin/productos' , adminProductosRoutes);
+
+
+app.use((req, res, next) => {res.status(404).send("la pagina no existe")});
+
+
+
+
+const PORT = 3000;
+
+app.listen(PORT,async() => {
+  try {
+    await sequelize.authenticate()
+  } catch (error) {
+    console.log(error);
+  }
+
+  console.log(`http://localhost:${PORT}`);
+});
+
+// app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
---
+++ src/controllers/admin/productosController.js
@@ -0,0 +1,75 @@
+const path = require("path");
+const sharp = require('sharp');
+
+const { validationResult }= require("express-validator");
+
+const model = require("../../models/Producto");
+
+
+
+
+const index = async (req,res) =>{
+   try {
+const productos = await model.findAll()
+res.render("admin/index", {productos});
+}
+catch(error){
+    res.status(500).send(error)
+}
+
+
+   };
+    
+
+const create =(req, res) => {
+  
+    res.render('admin/create');
+
+};
+
+// const create1 =(req, res) =>{
+//     res.sendFile(path.resolve(__dirname, "../../views/admin/create.ejs"));
+// };
+
+const store = (req, res) =>{
+    console.log(req.body, req.file);
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.render("admin/create", {
+            // values: req.body,
+            errors: errors.array(),
+        });
+    }
+
+    if (req.file) {
+    sharp(req.file.buffer)
+    .resize(300)
+    .toFile(path.resolve(__dirname, '../../../public/upload/image.jpg'))
+    .catch((err) => console.log(err));
+    
+     }
+
+    res.send("Producto creado");
+};
+
+
+const update =(req, res) =>{
+    console.log(req.params,req.body)
+    res.send("Producto modificado");};
+
+
+    const destroy =(req, res) =>{
+        console.log(req.params)
+        res.send("Producto borradoo");
+    
+};
+module.exports = {
+    index,
+    store,
+    update,
+    destroy,
+    create,
+    
+};
