7cb78d467670bfa305555b041e70b2b00dc16205
Ariel Galeppi <arielgaleppi@gmail.com>
2023-12-10 10:11:57 -0300

    login y logout OK

--- index.js
+++ index.js
@@ -5,6 +5,31 @@ const app = express();
 const path = require("path");
 const methodOverride = require("method-override");
 const sequelize = require('./src/models/connect');
+const session = require("cookie-session");
+
+
+// const session = require("express-session") ;
+
+// app.use(
+//   session({
+//     secret : "Milo2019",
+//     resave: false,
+//     saveUninitialized: false,
+//   })
+// );
+
+app.use(
+  session({
+    keys:["milo2019" , "Milo2020"]
+  })
+);
+
+const isLogin = (req, res, next) => {
+  if(!req.session.userId){
+    return res.redirect("/login");
+  }
+  next();
+}
 
 app.set("view engine" , "ejs");
 app.set("views", "./src/views");
@@ -21,7 +46,7 @@ const mainRoutes = require('./src/routes/mainRoutes');
 app.use(mainRoutes);
 
 const adminProductosRoutes = require("./src/routes/admin/productosRoutes");
-app.use('/admin/productos' , adminProductosRoutes);
+app.use('/admin/productos' , isLogin, adminProductosRoutes);
 
 
 app.use((req, res, next) => {
--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,4 +1,6 @@
+const bcryptjs = require("bcryptjs");
 const { validationResult}= require("express-validator");
+
 const model = require('../models/User');
 const register = (req, res) => {
     res.render("auth/register");
@@ -32,17 +34,50 @@ res.redirect("/");
 const login= (req, res) => {
     res.render('auth/login')
 }
-const postLogin= (req, res) => {
-    const errors =validationResult(req);
 
+const postLogin = async (req, res) => {
+    const errors = validationResult(req);
+  
     if (!errors.isEmpty()) {
-        return res.render("auth/login" , {
-            values :req.body,
-            errors: errors.array(),
+      return res.render("auth/login", {
+        values: req.body,
+        errors: errors.array(),
+      });
+    }
+  
+    try {
+      const user = await model.findOne({
+        where: {
+          email: req.body.email,
+        },
+      });
+  
+      if (!user) {
+        res.render("auth/login", {
+          values: req.body,
+          errors: [{ msg: "El correo y/o contraseña son incorrectos (email)" }],
         });
+      } else if (!(await bcryptjs.compare(req.body.password, user.password))) {
+        res.render("auth/login", {
+          values: req.body,
+          errors: [
+            { msg: "El correo y/o contraseña son incorrectos (password)" },
+          ],
+        });
+      } else {
+        req.session.userId = user.id;
+  
+        res.redirect("/");
+      }
+    } catch (error) {
+      console.log(error);
+      res.send(error);
+    }
+  };
+const logout = (req, res) =>{
+    req.session= null;
+    res.redirect("/");
 }
-};
-const logout = (req, res) =>{}
 
 
 module.exports = {
